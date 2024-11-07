571a7fba6c623893d0cc36222586a809e5848669
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-11-07 23:40:18 +0100

    hydrangea-screen-monitor 1.0.0 (new formula)

---
+++ Formula/h/hydrangea-screen-monitor.rb
@@ -0,0 +1,28 @@
+class HydrangeaScreenMonitor < Formula
+  include Language::Python::Virtualenv
+
+  desc "For safely monitoring screens"
+  homepage "https://github.com/Zeyu-Xie/Hydrangea-Screen-Monitor"
+  url "https://github.com/Zeyu-Xie/Hydrangea-Screen-Monitor/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "da435431d0ab4465dc69791fb5074b4a9234b7107b6d032239c668a58a8dbbe1"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install "pyinstaller"
+    venv.pip_install "macholib"
+    venv.pip_install "altgraph"
+    system libexec/"bin/pyinstaller",
+      "--onefile",
+      "--name", "hydrangea-screen-monitor",
+      "--distpath=#{libexec}/bin",
+      buildpath/"hydrangea-screen-monitor.py"
+    bin.install libexec/"bin/hydrangea-screen-monitor"
+  end
+
+  test do
+    assert_match "hydrangea-screen-monitor 1.0.0", shell_output("#{bin}/hydrangea-screen-monitor -v")
+  end
+end
